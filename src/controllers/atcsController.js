f2f6eaf35346446f229fee9428a536d8c6f14c2a
Irfan Akbari Habibi <irfanakbarihabibi18@gmail.com>
2023-06-10 08:16:31 +0700

    update CRUD endpoint for admin dashboard

--- src/controllers/atcsController.js
+++ src/controllers/atcsController.js
@@ -1,6 +1,21 @@
 import ATCSModel from '../models/atcsModel.js'
 
 const ATCSController = {
+    get: async (req, res) => {
+        try {
+            const atcs = await ATCSModel.findAll();
+
+            res.json({
+                status: 200,
+                atcs
+            }).status(200)
+        } catch (e) {
+            res.json({
+                status: 500,
+                message: e.message
+            }).status(500)
+        }
+    },
     post: async (req, res) => {
         const {nama_atcs, lat, long, id_kota, stream_url, is_monitoring} = req.body
         try {
